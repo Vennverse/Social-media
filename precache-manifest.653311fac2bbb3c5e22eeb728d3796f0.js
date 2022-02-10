47e7a81bb521c91547540dfe391a642919d8fadd
Vennverse <shubhamdubexskd2001@gmail.com>
2022-02-10 16:14:44 +0000

    Updates

---
+++ precache-manifest.653311fac2bbb3c5e22eeb728d3796f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4d9221f89dcd16c07edc82f35aed0a",
+    "url": "/Vennverse/index.html"
+  },
+  {
+    "revision": "30d531a66267dbc1336a",
+    "url": "/Vennverse/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "b7fe40f8e0a9cddf2431",
+    "url": "/Vennverse/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "30d531a66267dbc1336a",
+    "url": "/Vennverse/static/js/2.cf67b2b8.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/Vennverse/static/js/2.cf67b2b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7fe40f8e0a9cddf2431",
+    "url": "/Vennverse/static/js/main.1c145cc7.chunk.js"
+  },
+  {
+    "revision": "831174f96068d1568a1f",
+    "url": "/Vennverse/static/js/runtime-main.8f58ec3a.js"
+  }
+]);
