15e5859cac78c7da8f7f3b575c5f52d575e360ec
Shubham <shubhamdubexskd2001@gmail.com>
2022-02-10 20:01:08 +0530

    commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Identicon from 'identicon.js';
+
+
+class Navbar extends Component {
+
+  render() {
+    return (
+      
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      
+    </nav>
+    
+    );
+  }
+}
+
+export default Navbar;
